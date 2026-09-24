d0084e221745f37f033734b3f1d44ea3cee951c6
Carlos Domingos <99830819+CarlosDevITT@users.noreply.github.com>
2026-09-24 16:57:26 -0300

    chore(tank): refresh casual stability build

--- src/games/tank/index.js
+++ src/games/tank/index.js
@@ -1,5 +1,5 @@
 import { TankControls } from "./tank-controls.js?v=20260924-66";
-import { TankEngine } from "./tank-engine.js?v=20260924-77";
+import { TankEngine } from "./tank-engine.js?v=20260924-80";
 import { AuthoritativeTankEngine } from "./tank-authoritative-engine.js?v=20260924-72";
 import { GameServerClient } from "../../services/game-server.js?v=20260924-62";
 import { buildTankMatchPayload,resolveTankWinner } from "./tank-rules.js?v=20260924-62";
