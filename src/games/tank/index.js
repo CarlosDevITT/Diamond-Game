969681372bca1213bcc36d0e99ef4ad3e946939d
Carlos Domingos <99830819+CarlosDevITT@users.noreply.github.com>
2026-09-24 13:35:00 -0300

    chore(tank): load pathfinding bot engine

--- src/games/tank/index.js
+++ src/games/tank/index.js
@@ -1,5 +1,5 @@
 import { TankControls } from "./tank-controls.js?v=20260924-66";
-import { TankEngine } from "./tank-engine.js?v=20260924-75";
+import { TankEngine } from "./tank-engine.js?v=20260924-76";
 import { AuthoritativeTankEngine } from "./tank-authoritative-engine.js?v=20260924-72";
 import { GameServerClient } from "../../services/game-server.js?v=20260924-62";
 import { buildTankMatchPayload,resolveTankWinner } from "./tank-rules.js?v=20260924-62";
